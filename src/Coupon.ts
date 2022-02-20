24d00b52cc68609749f0a8305fa4e5ba2a844d3d
Renato Oliveira <oliversieto@gmail.com>
2022-02-20 16:30:50 -0400

    feat: create Order class and test

---
+++ src/Coupon.ts
@@ -0,0 +1,4 @@
+export default class Coupon {
+  constructor(readonly code: string, readonly percentage: number) {
+  }
+}
---
+++ src/Item.ts
@@ -0,0 +1,4 @@
+export default class Item {
+  constructor(readonly idItem: number, readonly category: string, readonly description: string, readonly price: number) {
+  }
+}
