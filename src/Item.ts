44971fe09b61a6103310855feeca4c0abf26004c
Renato Oliveira <oliversieto@gmail.com>
2022-02-28 19:41:40 -0400

    feat: freight, dimension and coupon

--- src/Item.ts
+++ src/Item.ts
@@ -1,4 +1,21 @@
+import Dimension from "./Dimension";
+
 export default class Item {
-  constructor(readonly idItem: number, readonly category: string, readonly description: string, readonly price: number) {
+  
+  constructor(readonly idItem: number, readonly category: string, readonly description: string, readonly price: number, readonly dimension?: Dimension, readonly weight?: number) {
+  }
+
+  getVolume(): number {
+    if (this.dimension) {
+      return this.dimension.getVolume();
+    }
+    return 0;
+  }
+
+  getDensity(): number {
+    if (this.weight && this.dimension) {
+      return this.weight / this.dimension.getVolume();
+    }
+    return 0;
   }
 }
